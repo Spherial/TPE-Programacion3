beed3d0bb1896049f92524d0a0d1aa866acf7583
AgusCuculich <cuculichagustina@gmail.com>
2025-06-18 01:24:23 -0300

    Greedy explicado

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,11 @@ public class Main {
 		optimizarMaquinasGreedy(maquinas, lector.getPiezasTotales());
     }
 
+	/*
+	* Descripción del algoritmo
+	*
+	* */
+
 	public static ArrayList<Maquina> optimizarMaquinasBacktrack(ArrayList<Maquina> maquinas, int piezasTotales){
 		ArrayList<Maquina> solucion = new ArrayList<>();
 
@@ -62,23 +67,26 @@ public class Main {
 		}
 	}
 
-	public static int calcularPiezas(ArrayList<Maquina> solucion) {
-		int suma = 0;
-		for(Maquina m : solucion) {
-			suma+= m.getPiezas();
-		}
-		return suma;
-	}
 
-	public static boolean solucionHallada(ArrayList<Maquina> solucion, int piezasTotales) {
-		int piezas = calcularPiezas(solucion);
-		return piezas == piezasTotales;
-	}
-
-	public static boolean factible(ArrayList<Maquina> solucion, Maquina candidato, int piezasTotales) {
-		int piezas = calcularPiezas(solucion) + candidato.getPiezas();
-		return piezas <= piezasTotales;
-	}
+	/**
+	 * Descripción del algoritmo:
+	 * 1) La función recibe un ArrayList de máquinas previamente ordenadas en base a la cantidad de piezas que producen
+	 * (de mayor a menor).
+	 * 2) Mientras el ArrayList de máquinas no esté vacío y no hayamos encontrado una solución, se realizan los
+	 * siguientes pasos:
+	 *    a) Seleccionamos un candidato:
+	 *       - El candidato será siempre la primera máquina del ArrayList, ya que es la que más piezas produce, y por
+	 *       ende, aquella que nos acerque más rápidamente a nuestro objetivo de fabricación.
+	 *    b) Evaluamos si el candidato es factible:
+	 *       - Una máquina es factible si, al sumar la cantidad de piezas que produce a las piezas acumuladas hasta el
+	 *       momento, no se supera el total requerido.
+	 *       - Si el candidato es factible, lo añadimos al ArrayList "solución".
+	 *       - Si no es factible, eliminamos la primera máquina (actual candidato).
+	 * 3) Finalización:
+	 *    - Si encontramos una solución (un conjunto de máquinas que producen la cantidad deseada de piezas), mostramos
+	 *    las métricas por consola y devolvemos la solución.
+	 *    - Si no encontramos ninguna solución posible, devolvemos null.
+	 */
 
 	public static ArrayList<Maquina> optimizarMaquinasGreedy(ArrayList<Maquina> maquinas, int piezasTotales) {
 		ArrayList<Maquina> solucion = new ArrayList<>();
@@ -103,7 +111,28 @@ public class Main {
 			System.out.println("Candidatos considerados: " + metricaCandidatos);
 			return solucion;
 		} else {
+			System.out.println("No se halló solución");
 			return null;
 		}
+	}
+
+	// Funciones utilizadas en greedy
+
+	public static int calcularPiezas(ArrayList<Maquina> solucion) {
+		int suma = 0;
+		for(Maquina m : solucion) {
+			suma+= m.getPiezas();
+		}
+		return suma;
+	}
+
+	public static boolean solucionHallada(ArrayList<Maquina> solucion, int piezasTotales) {
+		int piezas = calcularPiezas(solucion);
+		return piezas == piezasTotales;
+	}
+
+	public static boolean factible(ArrayList<Maquina> solucion, Maquina candidato, int piezasTotales) {
+		int piezas = calcularPiezas(solucion) + candidato.getPiezas();
+		return piezas <= piezasTotales;
 	}
 }
